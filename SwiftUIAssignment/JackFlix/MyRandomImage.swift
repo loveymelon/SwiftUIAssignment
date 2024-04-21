900e52a873d1b9e5083246af772a45ec508cc02b
loveymelon <52749067+loveymelon@users.noreply.github.com>
2024-04-21 23:39:05 +0900

    My Random Image 구성

---
+++ SwiftUIAssignment/JackFlix/MyRandomImage.swift
@@ -0,0 +1,57 @@
+//
+//  MyRandomImage.swift
+//  SwiftUIAssignment
+//
+//  Created by 김진수 on 4/21/24.
+//
+
+import SwiftUI
+
+struct MyRandomImage: View {
+    
+    let items = ["첫","두","세","네"]
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.flexible(minimum: 0, maximum: .infinity))],spacing: 10 ,content: {
+                    ForEach(items, id: \.self) { num in
+                        makeSection(content: ScrollView(.horizontal) {
+                            VStack(content: {
+                                LazyHGrid(rows: [GridItem(.flexible(minimum: 0, maximum: .infinity))], content: {
+                                    ForEach(items, id: \.self) { _ in
+                                        NavigationLink {
+                                            NextView()
+                                        } label: {
+                                            RandomImageView()
+                                                .frame(width: 100, height: 150)
+                                                .clipShape(.rect(cornerRadius: 10))
+                                        }
+                                    }
+                                })
+                            })
+                        }, header: num)
+                    }
+                    
+                }
+                )}.navigationTitle("My Random Image")
+            .padding(.leading, 20)
+        }
+    }
+}
+
+func makeSection(content: some View, header: String) -> some View {
+    return Section {
+        content
+    } header: {
+        HStack(content: {
+            Text("\(header)번째 섹션")
+            Spacer()
+        })
+    }
+}
+
+
+#Preview {
+    MyRandomImage()
+}
---
+++ SwiftUIAssignment/JackFlix/NextView.swift
@@ -0,0 +1,37 @@
+//
+//  NextView.swift
+//  SwiftUIAssignment
+//
+//  Created by 김진수 on 4/21/24.
+//
+
+import SwiftUI
+
+struct NextView: View {
+    
+    let url = URL(string: "https://picsum.photos/200/300")
+    
+    var body: some View {
+        AsyncImage(url: url) { data in
+            switch data {
+            case .empty:
+                ProgressView()
+                    .frame(width: 100, height: 100)
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 150)
+                    .clipShape(.rect(cornerRadius: 10))
+            case .failure(let error):
+                Image(systemName: "star")
+            @unknown default:
+                Image(systemName: "star")
+            }
+        }
+    }
+}
+
+#Preview {
+    NextView()
+}
